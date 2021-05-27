ab8c208145ee3c5e89968029b74cc16d4e0916f0
Iliyan Markov <ilkomarkov000@gimail.com>
2021-05-27 22:34:36 +0300

    minor changes

--- server/app.js
+++ server/app.js
@@ -1,53 +1,52 @@
-const { WSAEWOULDBLOCK } = require('constants')
-const WebSocket = require('ws')
+const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8989 })
 
 const users = []
 
-const broadcast = (data, webSocket) => {
-    wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN && client !== webSocket) {
-            client.send(JSON.stringify(data))
-        }
-    })
+const broadcast = (data, ws) => {
+	wss.clients.forEach((client) => {
+		if (client.readyState === WebSocket.OPEN && client !== ws ) { 
+			client.send(JSON.stringify(data))
+		}
+	})
 }
 
-wss.on('connection', (webSocket) => {
-    let index
-    webSocket.on('message', (message) => {
-        const data = JSON.parse(message)
-        switch (data.type) {
-            case 'ADD_USER' : {
-                index = users.length
-                users.push({ name: data.name, id: index + 1 })
-                webSocket.send(JSON.stringify({
-                    type: 'USERS_LIST',
-                    users
-                }))
-                broadcast({
-                    type: 'USERS_LIST',
-                    users
-                }, webSocket)
-                break
-            }
-            case 'ADD_MESSAGE':
-                broadcast({
-                    type: 'ADD_MESSAGE',
-                    message: data.message,
-                    author: data.author
-                }, webSocket)
-                break
-            default:
-                break
-        }
-    })
+wss.on('connection', (ws) => {
+	let index
+	ws.on('message', (message) => {
+		const data = JSON.parse(message)
+		switch (data.type) {
+			case 'ADD_USER': {
+				index = users.length
+				users.push({ name: data.name, id: index + 1})
+				ws.send(JSON.stringify({
+					type: 'USERS_LIST',
+					users
+				}))
+				broadcast({
+					type: 'USERS_LIST',
+					users
+				}, ws)
+				break
+			}
+			case 'ADD_MESSAGE':
+				broadcast({
+					type: 'ADD_MESSAGE',
+					message: data.message,
+					author: data.author
+				}, ws)
+				break
+			default:
+				break
+		}
+	})
 
-    webSocket.on('close', () => {
-        users.splice(index, 1)
-        broadcast({
-            type: 'USERS_LIST',
-            users
-        }, webSocket)
-    })
+	ws.on('close', () => {
+		users.splice(index, 1)
+		broadcast({
+			type: 'USERS_LIST',
+			users
+		}, ws)
+	})
 })
